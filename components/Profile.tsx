de73d48d5d8a5bc7d8652fa9a07e4c0452845307
Tobi Idowu <idtobi8@gmail.com>
2024-03-28 13:47:05 +0100

    Updated Profile & Item

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -4,12 +4,10 @@ import avatar from "../assets/avatar.png";
 
 const Profile = () => {
   return (
-    <div className="w-full bg-alternateBlue h-[300px] rounded-br-3xl rounded-tr-3xl flex items-center justify-center gap-3 flex-col">
+    <div className="w-full py-3 bg-alternateBlue rounded-br-3xl rounded-tr-3xl flex items-center justify-center gap-3 flex-col">
       <Image src={avatar} alt="avatar" />
       <h1 className="text-white text-2xl font-medium">ALEX JOHNSON</h1>
       <p className="text-white text-sm">alex.johnson@gmail.com</p>
-      <p></p>
-      <i className="bi-alarm text-white"></i>
     </div>
   );
 };
