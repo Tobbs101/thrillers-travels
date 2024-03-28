8f6fb0da07372c72bd69a9996d1f1b93a2f87a87
Tobi Idowu <idtobi8@gmail.com>
2024-03-28 21:50:36 +0100

    Update CurrentTrips.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import PageLayout from "@/components/PageLayout";
 import Aside from "@/components/Aside";
 import TripSelector from "@/components/TripSelector";
 import Filter from "@/components/Filter";
+import CurrentTrips from "@/components/CurrentTrips";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Aside>
         <TripSelector />
         <Filter />
+        <CurrentTrips />
       </Aside>
     </PageLayout>
   );
---
+++ components/CurrentTrips.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CurrentTrips = () => {
+  return (
+    <div className="w-full gap-4 flex items-start justify-center border border-red-500">
+      <div className="flex-[4] border border-blue-500 rounded-3xl p-3"></div>
+      <div className="flex-1 border border-blue-500 rounded-3xl p-3"></div>
+    </div>
+  );
+};
+
+export default CurrentTrips;
