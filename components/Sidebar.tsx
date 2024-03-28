e21d26d7522d34e7b7728e1e549f1271285a40fc
Tobi Idowu <idtobi8@gmail.com>
2024-03-28 22:38:53 +0100

    Updated Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,19 +6,21 @@ import Image from "next/image";
 
 const Sidebar = () => {
   return (
-    <section className="border p-0 border-customBlue bg-customBlue flex-1 rounded-br-3xl rounded-tr-3xl">
-      <Profile />
-      <Links />
-      <div className="w-[80%] mx-auto flex items-center flex-col justify-center gap-2">
-        <h1 className="text-customBrown text-[13px] font-medium">
-          ACTIVE USERS
-        </h1>
-        <Image
-          src={bg_footer}
-          alt="background footer"
-          className="h-[150px] object-contain"
-        />
-      </div>
+    <section className="border pr-2 p-0 border-lightBlue bg-lightBlue flex-1 rounded-br-3xl rounded-tr-3xl">
+      <section className="border overflow-hidden bg-customBlue rounded-br-3xl rounded-tr-3xl h-full">
+        <Profile />
+        <Links />
+        <div className="w-[80%] mx-auto flex items-center flex-col justify-center gap-2">
+          <h1 className="text-customBrown text-[13px] font-medium">
+            ACTIVE USERS
+          </h1>
+          <Image
+            src={bg_footer}
+            alt="background footer"
+            className="h-[150px] object-contain"
+          />
+        </div>
+      </section>
     </section>
   );
 };
