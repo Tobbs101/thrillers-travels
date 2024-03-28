5b6f1f719348a334dbc967c76bdff58af9f40957
Tobi Idowu <idtobi8@gmail.com>
2024-03-28 16:30:30 +0100

    Updated Links.tsx

--- components/Links.tsx
+++ components/Links.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import { routes } from "@/routes";
 import { Route } from "@/interfaces";
 import Each from "./Each";
