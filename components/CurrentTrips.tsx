88175e7d7d7e934d72b90411ccdb8f0449c129d0
Tobi Idowu <idtobi8@gmail.com>
2024-03-28 22:04:35 +0100

    Updated CurrentTrips.tsx

--- components/CurrentTrips.tsx
+++ components/CurrentTrips.tsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const CurrentTrips = () => {
   return (
-    <div className="w-full gap-4 flex items-start justify-center border border-red-500">
-      <div className="flex-[4] border border-blue-500 rounded-3xl p-3"></div>
-      <div className="flex-1 border border-blue-500 rounded-3xl p-3"></div>
+    <div className="w-full gap-4 flex items-start justify-center">
+      <div className="flex-[4] border border-white bg-white rounded-3xl p-3"></div>
+      <div className="flex-[2] border border-customBlue bg-customBlue rounded-3xl p-3">
+        <div className="border border-red-500 flex items-center justify-between">
+          <span className="text-white flex items-center justify-center flex-col text-sm">
+            <p>FROM</p>
+            <p>JFK</p>
+          </span>
+          <p className="text-[10px] font-normal text-white">NON-STOP</p>
+          <span className="text-white flex items-center justify-center flex-col text-sm">
+            <p>TO</p>
+            <p>BOM</p>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
