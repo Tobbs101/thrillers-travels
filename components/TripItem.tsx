79346f452ec2987249d6d53e2a97f28335b86fad
Tobi Idowu <idtobi8@gmail.com>
2024-03-29 07:37:47 +0100

    Updated TripItem.tsx

--- components/TripItem.tsx
+++ components/TripItem.tsx
@@ -2,12 +2,45 @@ import { trip_item } from "@/interfaces";
 import React from "react";
 import Image from "next/image";
 import { trip_data } from "./utils";
-import { trip_item_style } from "./utils";
+import { trip_item_style, trip_pseudo_classes } from "./utils";
+import { Show } from "./Show";
+import Each from "./Each";
 
 const TripItem = ({ item, index }: { item: trip_item; index: number }) => {
   return (
     <div className={trip_item_style(trip_data.length, index + 1)}>
       <Image src={item.img} alt={item.airline} />
+      <Show>
+        <Show.When isTrue={index + 1 !== trip_data.length}>
+          <Each
+            of={trip_pseudo_classes}
+            render={(item: string, index: number) => (
+              <div key={index} className={item}></div>
+            )}
+          />
+        </Show.When>
+      </Show>
+      <div className="flex items-center justify-center gap-10">
+        <span className="flex items-center justify-center flex-col">
+          <h1 className="text-alternateBlue font-semibold text-lg">JFK</h1>
+          <p className="text-alternateBlue text-[11px] font-medium">13:00</p>
+        </span>
+        <span className="flex items-center justify-center flex-col">
+          <p className="text-alternateBlue text-[10px]">QATAR AIRWAYS</p>
+          <p className="text-customBrown text-[11px] font-medium">11H 20M</p>
+          <p className="text-alternateBlue text-sm font-medium">NON-STOP</p>
+        </span>
+        <span className="flex items-center justify-center flex-col">
+          <h1 className="text-alternateBlue font-semibold text-lg">BOM</h1>
+          <p className="text-alternateBlue text-[11px] font-medium">14:20</p>
+        </span>
+      </div>
+      <div className="flex items-center justify-center flex-col gap-3">
+        <p className="text-sm font-semibold text-alternateBlue">{item.price}</p>
+        <button className="text-[8px] font-semibold border border-customBrown bg-customBrown text-white flex items-center justify-center gap-3 py-2 px-6 rounded-full">
+          BOOK NOW
+        </button>
+      </div>
     </div>
   );
 };
--- components/utils.tsx
+++ components/utils.tsx
@@ -4,6 +4,11 @@ import qatar from "../assets/qatar.png";
 
 export const pseudo_items = ["before", "after", "before2", "after2"];
 
+export const trip_pseudo_classes = [
+  "absolute rounded-full h-7 w-7 bottom-[-14px] left-[-15px] bg-lightBlue",
+  "absolute rounded-full h-7 w-7 bottom-[-14px] right-[-15px] bg-lightBlue",
+];
+
 export const trip_options = [
   { id: 1, label: "ONE WAY", value: "one-way" },
   { id: 2, label: "ROUND TRIP", value: "round-trip" },
@@ -49,7 +54,7 @@ export const trip_option_p = (selectedTrip: string, value: string) => {
 
 export const trip_item_style = (trips_length: number, index: number) => {
   if (trips_length === index)
-    return "p-3 h-[120px] flex items-center justify-between";
+    return "p-3 h-[120px] flex items-center justify-between relative";
 
-  return "border-b border-dashed border-slate-300 p-3 h-[120px] flex items-center justify-between";
+  return "border-b relative border-dashed border-slate-300 p-3 h-[120px] flex items-center justify-between";
 };
